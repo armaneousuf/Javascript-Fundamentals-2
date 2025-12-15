f23231a60f66f4c472b711792be6dc3aa81acf7f
armaneousuf <armaneousuf97@gmail.com>
2025-12-15 13:04:09 +0600

    completed task-02

--- CODING_PROBLEM_02/tasks/task-02.js
+++ CODING_PROBLEM_02/tasks/task-02.js
@@ -9,3 +9,17 @@ Goal:
 Write a function that returns the name with the smallest length.
 */
 
+function smallestInLength(friends) {
+    // console.log(friends);
+    let smallest = friends[0];
+    for (const friend of friends) {
+        // console.log(friend);
+        if (friend.length < smallest.length) {
+            smallest = friend;
+        }
+    }
+    return smallest;
+}
+
+const friends = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+console.log(smallestInLength(friends));
---
+++ CODING_PROBLEM_02/tasks/task-03.js
@@ -0,0 +1,16 @@
+/* 
+Task-3:
+Your task is to calculate the total budget required to buy electronics.
+
+Prices:
+- laptop = 35000 tk
+- tablet = 15000 tk
+- mobile = 20000 tk
+
+Input:
+The function will receive the number of laptops, tablets, and mobiles.
+
+Goal:
+Write a function named calculateElectronicsBudget that returns the total cost.
+*/
+
